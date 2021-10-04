d3ae5558b8974cb9a51715369a737d9dbb385938
DCFukuda <kakeru.fukuda.0228@gmail.com>
2021-10-04 12:15:43 +0900

    add new file

---
+++ ABC221A.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DIVA_INTERN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if(string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("入力なし");
+                return;
+            }
+
+            string[] inputArr = input.Split(" ");
+
+            if(inputArr.Length != 2)
+            {
+                Console.WriteLine("空白区切りで入力してください");
+                return;
+            }
+
+            if(!int.TryParse(inputArr[0],out int a) || !int.TryParse(inputArr[1],out int b))
+            {
+                Console.WriteLine("整数値を入力してください");
+                return;
+            }
+
+            Console.WriteLine(Math.Pow(32, a - b));
+        }
+    }
+}
---
+++ ABC221B.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DIVA_INTERN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string v = Console.ReadLine();
+
+            if(string.IsNullOrEmpty(s) || string.IsNullOrEmpty(v))
+            {
+                Console.WriteLine("入力がありません");
+                return;
+            }
+
+            if(s.Equals(v)) {
+                Console.WriteLine("Yes");
+                return;
+            }
+
+            int sLen = s.Length;
+            string s1,sRep1,sRep2,s4,stmp;
+            for (var i = 0; i < sLen - 1; i++)
+            {
+                s1 = s.Substring(0, i);
+                sRep1 = s.Substring(i, 1);
+                sRep2 = s.Substring(i+1, 1);
+                if(i == sLen-2)
+                {
+                    s4 = "";
+                }else
+                {
+                    s4 = s.Substring(i+2);
+                }
+                
+                stmp = s1 + sRep2 + sRep1 + s4;
+                
+                if(stmp.Equals(v)) 
+                {
+                    Console.WriteLine("Yes");
+                    return;
+                }
+            }
+
+            Console.WriteLine("No");
+        }
+    }
+}
